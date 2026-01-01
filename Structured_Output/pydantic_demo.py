e9a37a6b10d166f0679715a649fce39d81ace0c9
Suraj Bist <bishtsuraj087@gmail.com>
2026-01-01 20:44:55 +0530

    learn about different types of Structured_outputs

---
+++ Structured_Output/pydantic_demo.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel , EmailStr , Field
+from typing import Optional
+
+class Student(BaseModel):
+
+    name: str ='Suraj'
+    age: Optional[int]= None
+    email: EmailStr
+    cgpa: float = Field(gt=0,lt=10,default=5,description="A decimal value representing the cgpa of the student")
+
+new_student ={"age":32, "email": 'suraj@gmail.com'}
+
+student = Student(**new_student)
+
+print(student.model_dump())
+
