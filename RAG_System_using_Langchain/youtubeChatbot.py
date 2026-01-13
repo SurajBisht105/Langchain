e085a05c563d637d2872211a6b4adfcc44e61315
Suraj Bist <bishtsuraj087@gmail.com>
2026-01-13 21:59:40 +0530

    xyz

--- RAG_System_using_Langchain/youtubeChatbot.py
+++ RAG_System_using_Langchain/youtubeChatbot.py
@@ -1,4 +1,4 @@
-# ! pip Install -q youtube-transcript-api language-community langchain-openai faiss-cpu tiktoken python-dotenv
+# ! pip install -q youtube-transcript-api langchain-community langchain-openai faiss-cpu tiktoken python-dotenv
 
 import os
 
@@ -10,11 +10,61 @@ from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbe
 from langchain_core.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS
 
+
+
 video_id = "Gfr50f6ZBvo"   #   only the ID, not full URL
 try:
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"])
+    transcript_list = YouTubeTranscriptApi().fetch(video_id, languages=["en"])
 
-    transcript = " ".join(chunk ["text"] for chunk in transcript_list)
+    transcript = " ".join(chunk.text for chunk in transcript_list)
     print(transcript)
+
 except TranscriptsDisabled:
     print("No captions available for this video.")
+
+
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = splitter.create_documents([transcript])
+print(len(chunks))
+
+
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+vectorstore = FAISS.from_documents(chunks, embeddings)
+ 
+
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwarg={"k": 4})
+retriever.invoke("What is deepmind")
+
+
+
+llm=ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+prompt = PromptTemplate(
+    template="""
+    You are a helpful assistant.
+    Answer ONLY from the provided transcript context.
+    If the context is insufficient, just say you don't know.
+
+    {context}
+    Question: {question}
+""",
+input_variables= {'context', 'question'}
+)
+
+
+
+question = "is this topic of aliens discussed in this video? if yes then what was discussed"
+retrieved_docs = retriever.invoke(question)
+
+
+context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+
+final_prompt = prompt.invoke({"context": context_text ,"question":question})
+
+
+
+answer= llm.invoke(final_prompt)
+print(answer)
